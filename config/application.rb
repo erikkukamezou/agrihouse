0c9f948e98c9d7bcfa7654694c50e966bd555026
erikkukamezou <erikkukamezoudayo@yahoo.co.jp>
2021-08-28 10:12:18 +0000

    修正

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module Agrihouse
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
 
+    config.assets.initialize_on_precompile = false
+
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
