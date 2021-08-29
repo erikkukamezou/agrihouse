38dfb4a3ee2b84e1bd53c544cded78ec66cccaa0
erikkukamezou <erikkukamezoudayo@yahoo.co.jp>
2021-08-29 00:46:12 +0000

    deviseのletter_open9

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  ActionMailer::Base.delivery_method = :letter_opener_web
+  config.action_mailer.default_url_options = { host: 'arcane-mesa-04338.herokuapp.com'}
 end
